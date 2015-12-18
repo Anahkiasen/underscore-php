1b0878faf48b80a3876a0258915ef077a664e2de
tamakiii <daichi_tamaki@voyagegroup.com>
2015-12-18 18:49:30 +0900

    impl Arrays::indexBy()

--- src/Methods/CollectionMethods.php
+++ src/Methods/CollectionMethods.php
@@ -370,4 +370,27 @@ abstract class CollectionMethods
             unset($collection[$key]);
         }
     }
+
+    /**
+     * Given a list, and an iteratee function that returns
+     * a key for each element in the list (or a property name),
+     * returns an object with an index of each item.
+     * Just like groupBy, but for when you know your keys are unique.
+     *
+     * @param array $array
+     * @param mixed $key
+     * @return array
+     */
+    public static function indexBy(array $array, $key)
+    {
+        $results = [];
+
+        foreach ($array as $a) {
+            if (isset($a[$key])) {
+                $results[$a[$key]] = $a;
+            }
+        }
+
+        return $results;
+    }
 }
